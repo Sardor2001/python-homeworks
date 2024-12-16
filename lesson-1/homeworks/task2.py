21c89761675f18d3b2ec4aaaa1453fc44c210866
Sardor2001 <sardorsaitkulov716@gmail.com>
2024-12-16 10:37:18 +0500

    second task

---
+++ lesson-1/homeworks/task2.py
@@ -0,0 +1,6 @@
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+num3 = float(input("Third number: "))
+
+print(f"The largest is {max(num1, num2, num3)}")
+print(f"The smallest is {min(num1, num2, num3)}")
