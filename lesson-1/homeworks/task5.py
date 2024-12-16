a38402457a73278208e6f69ae51ed734f22f7047
Sardor2001 <sardorsaitkulov716@gmail.com>
2024-12-16 10:50:10 +0500

    last changes

---
+++ lesson-1/homeworks/task5.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
---
+++ lesson-1/homeworks/task7.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number: "))
+
+result = f"{number} is even." if number % 2 == 0 else f"{number} is odd."
+
+print(result)
