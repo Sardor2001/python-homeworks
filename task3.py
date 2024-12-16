d30c53753da1b975c415d19b644e5f9999b99eb6
Sardor2001 <sardorsaitkulov716@gmail.com>
2024-12-16 10:40:00 +0500

    task3 commit

---
+++ task3.py
@@ -0,0 +1,7 @@
+kilometers = float(input("Enter distance in kilometers: "))
+
+meters = kilometers * 1000
+centimeters = kilometers * 100_000
+
+print(f"{meters} meters")
+print(f"{centimeters} centimeters")
