767d2cf677580bb9c320752d2615ac98b55fb4fe
Sardor2001 <sardorsaitkulov716@gmail.com>
2024-12-20 14:25:31 +0500

    lesson 4 task3

--- lesson-4/homeworks/task3.py
+++ lesson-4/homeworks/task3.py
@@ -1,4 +1,19 @@
+txt = input("Enter a string: ")
 
+vowels = 'aeiouAEIOU'
+
+mySet = set(vowels)
+result = []
+j = 0
+for i in range (0, len(txt)):
+    result.append(txt[i])
+    j+=1
+    if (j>=3) and not (txt[i] in mySet) and i!=len(txt)-1:
+        result.append('_')
+        mySet.add(txt[i])
+        j = 0
+
+print(''.join(str(x) for x in result))
 
 
 
