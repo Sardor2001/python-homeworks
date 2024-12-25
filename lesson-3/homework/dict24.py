f3bdb5fc506628e73bebfa51b23918c24ca14fe9
Sardor2001 <sardorsaitkulov716@gmail.com>
2024-12-26 02:18:01 +0500

    dict problems

---
+++ lesson-3/homework/dict24.py
@@ -0,0 +1,5 @@
+tuple_of_pairs = (('a', 1), ('b', 2), ('c', 3))
+
+result_dict = dict(tuple_of_pairs)
+
+print(result_dict)
---
+++ lesson-3/homework/dict25.py
@@ -0,0 +1,5 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+first_pair = next(iter(my_dict.items()))
+
+print(first_pair)
